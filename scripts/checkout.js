14bdf0556b6c0c6d1412b1432dd8a96b72b0be32
TapJc <brian209222@gmail.com>
2024-07-30 17:14:30 -0500

    Made Delivery Options Interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import {
   removeFromCart,
   updateCartQuantity,
   updateQuantity,
+  updateDeliveryOption,
 } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
@@ -70,7 +71,7 @@ cart.forEach((cartItem) => {
           <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${
             matchingProduct.id
           }">Save</span>
-          <span class="delete-quantity-link link-primary js-delete-link" data-product-id ="${
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
             matchingProduct.id
           }">
             Delete
@@ -104,7 +105,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option" 
+    data-product-id="${matchingProduct.id}" 
+    data-delivery-option-id="${deliveryOption.id}">
           <input type="radio"
             ${isChecked ? "checked" : ""}
             class="delivery-option-input"
@@ -168,5 +171,12 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     );
     container.remove();
     updateCartQuantity(".js-return-to-home-link");
+  });
+});
+
+document.querySelectorAll(".js-delivery-option").forEach((element) => {
+  element.addEventListener("click", () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });
