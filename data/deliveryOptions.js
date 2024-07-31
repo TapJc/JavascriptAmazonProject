bad68b1dadf57395b84e110787d30183f1fb6024
TapJc <brian209222@gmail.com>
2024-07-31 18:39:04 -0500

    Updated Delivery Dates Are Not On Weekends

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,3 +1,5 @@
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+
 export const deliveryOptions = [
   {
     id: "1",
@@ -26,4 +28,29 @@ export function getDeliveryOption(deliveryOptionId) {
   });
   // Default value if deliveryOption isn't found
   return deliveryOption || deliveryOptions[0];
+}
+
+function isWeekend(deliveryDate) {
+  return (
+    deliveryDate.format("dddd") === "Saturday" ||
+    deliveryDate.format("dddd") === "Sunday"
+  );
+}
+
+export function calculateDeliveryDate(deliveryOption) {
+  let deliveryDate = dayjs();
+  let remainingDays = deliveryOption.deliveryDays;
+
+  while (remainingDays > 0) {
+    let date = deliveryDate.add(1, "days");
+    deliveryDate = date;
+
+    if (!isWeekend(deliveryDate)) {
+      remainingDays--;
+    }
+  }
+
+  const dateString = deliveryDate.format("dddd, MMMM D");
+
+  return dateString;
 }
